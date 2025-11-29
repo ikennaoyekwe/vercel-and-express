d5c6a29f4155770084ca53b71efa07847dffb1a9
ikennaoyekwe <arash.internet@gmail.com>
2025-11-29 20:12:42 +0330

    added "Three.js" and a "Cube"

--- frontend/src/components/pages/PlayGround.jsx
+++ frontend/src/components/pages/PlayGround.jsx
@@ -1,11 +1,12 @@
 import React, {useEffect, useRef, useState} from "react";
 import CanvasTests from "../test_components/CanvasTests.jsx";
+import ThreejsCube from "../test_components/threejsCube.jsx";
 
 export default function PlayGround() {
 
     return (
         <>
-            <CanvasTests />
+            <ThreejsCube />
         </>
     )
 }
