81cd3c300a8489c0eaadbda9ba36c42d0d28a0ed
ikennaoyekwe <arash.internet@gmail.com>
2025-12-13 20:44:54 +0330

    just checking production

--- frontend/src/components/pages/MainPage.jsx
+++ frontend/src/components/pages/MainPage.jsx
@@ -21,10 +21,6 @@ export default function MainPage() {
 
     return (
         <div>
-            <div>
-                <h1>Changed TOO DEFAULT GIT PUSH</h1><br/>
-                <h1>{ip.country_name} | {ip.ip} | {ip.latitude} | {ip.longitude} </h1>
-            </div>
             <div id="lyrics" className="max-w-[70vw] md:max-w-[48vw] h-[50px] mx-0">
                 <Lyrics svgOpacity={svgOpacity}/>
             </div>
